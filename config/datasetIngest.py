3a2bcea197b3b98c2135d14e0122f187e48e4601
Krzysztof Findeisen <kfindeis@uw.edu>
2018-04-05 11:55:35 -0700

    Support config overrides for DatasetIngestTask.
    Despite not being a CmdLineTask, DatasetIngestTask will respect
    obs-package- and camera-specific override files following standard
    Stack conventions.

--- config/datasetIngest.py
+++ config/datasetIngest.py
@@ -1,20 +1,8 @@
-import os.path
-
-from lsst.utils import getPackageDir
-from lsst.obs.decam.ingest import DecamIngestTask
-
-obsConfigDir = os.path.join(getPackageDir('obs_decam'), 'config')
-
-config.dataIngester.retarget(DecamIngestTask)
-config.dataIngester.load(os.path.join(obsConfigDir, 'ingest.py'))
-
-config.calibIngester.load(os.path.join(obsConfigDir, 'ingestCalibs.py'))
 # Ignore wtmaps and illumcors
 # These data products may be useful in the future, but are not yet supported by the Stack
 # and will confuse the ingester
 config.calibBadFiles = ['*_fcw_*', '*_zcw_*', '*_ici_*']
 
-config.defectIngester.load(os.path.join(obsConfigDir, 'ingestCalibs.py'))
 config.defectTarball = 'defects_2014-12-05.tar.gz'
 
 config.refcats = {
