89347ec965d75d1e53048d36a00cf3d3e4f562b0
Krzysztof Findeisen <kfindeis@uw.edu>
2021-09-02 18:58:18 -0500

    Add ApVerifyWithFakes overrides.
    This includes extra config files for working around DM-30210. These files
    can be removed once templates work as expected.

---
+++ config/imageDifferenceWithFakes.py
@@ -0,0 +1,27 @@
+# Config override for lsst.pipe.tasks.imageDifference.ImageDifferenceTask
+# Templates are deepCoadds assembled with the CompareWarp algorithm
+
+# This file is only needed to provide the explicit "fakes_" connections names
+# required by DM-30210. Once that issue is resolved, this file can be removed
+# and the pipeline can use imageDifference.py plus a direct override
+# of fakesType.
+
+config.connections.fakesType = "fakes_"
+config.connections.coaddName = "deep"
+# These two lines can be removed once ImageDifference no longer supports Gen 2
+config.coaddName = config.connections.coaddName
+config.getTemplate.coaddName = config.connections.coaddName
+
+config.getTemplate.warpType = "direct"
+
+# TODO: redundant connection definitions workaround for DM-30210
+config.connections.exposure = "fakes_calexp"
+config.connections.coaddExposures = "fakes_deepCoadd"
+config.connections.dcrCoadds = "fakes_dcrCoadd"
+config.connections.outputSchema = "fakes_deepDiff_diaSrc_schema"
+config.connections.subtractedExposure = "fakes_deepDiff_differenceExp"
+config.connections.scoreExposure = "fakes_deepDiff_scoreExp"
+config.connections.warpedExposure = "fakes_deepDiff_warpedExp"
+config.connections.matchedExposure = "fakes_deepDiff_matchedExp"
+config.connections.diaSources = "fakes_deepDiff_diaSrc"
+# TODO: end DM-30210 workaround
