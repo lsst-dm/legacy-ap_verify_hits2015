db98ae8d93c0374272a408557971145e7f998cce
Krzysztof Findeisen <kfindeis@uw.edu>
2022-01-11 13:06:41 -0600

    Remove Gen 2->3 conversion configs.

--- config/convertRepo_calibs.py
+++
@@ -1,6 +0,0 @@
-# Config overrides for convert_gen2_repo_to_gen3.py
-
-config.datasetIgnorePatterns = ["raw", "*Coadd_skyMap", "ref_cat", "defects", "crosstalk"]
-
-# Already stored in convertRepo_templates.py
-config.doRegisterInstrument = False
--- config/convertRepo_templates.py
+++
@@ -1,7 +0,0 @@
-# Config overrides for convert_gen2_repo_to_gen3.py
-
-# Pass only templates and their supporting types
-config.datasetIncludePatterns = ["*Coadd*"]
-config.datasetIgnorePatterns = []
-for coaddType in ["deep", "goodSeeing", "dcr"]:
-    config.runs[f"{coaddType}Coadd"] = f"templates/{coaddType}"
