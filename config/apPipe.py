17e5aec80c41d5f7df1bfd785171de9a5c3a8747
Ian Sullivan <sullii@uw.edu>
2022-03-25 17:14:23 -0700

    Add getTemplate coaddName to connections

--- config/apPipe.py
+++ config/apPipe.py
@@ -14,6 +14,7 @@ config.ccdProcessor.isr.load(os.path.join(configDir, 'isr.py'))
 config.ccdProcessor.calibrate.load(os.path.join(configDir, 'calibrate.py'))
 
 # Use dataset's specific templates
+config.retrieveTemplate.load(os.path.join(configDir, 'retrieveTemplate.py'))
 config.differencer.load(os.path.join(configDir, 'imageDifference.py'))
 config.transformDiaSrcCat.load(os.path.join(configDir, 'transformDiaSrcCat.py'))
 config.diaPipe.load(os.path.join(configDir, 'diaPipe.py'))
---
+++ config/retrieveTemplate.py
@@ -0,0 +1,4 @@
+# Config override for lsst.ip.diffim.getTemplate.GetTemplateTask
+# Templates are deepCoadds assembled with the CompareWarp algorithm
+
+config.connections.coaddName = "deep"
