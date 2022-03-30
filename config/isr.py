42bef460b8ba5a606059b5b7763ebe4d86307c01
Krzysztof Findeisen <kfindeis@uw.edu>
2022-03-29 19:06:00 -0500

    Merge ISR task config.

--- config/isr.py
+++
@@ -1,8 +0,0 @@
-# Config override for lsst.ip.isr.IsrTask
-
-# This dataset contains CP calibs, not regular ones
-config.connections.bias = "cpBias"
-config.connections.flat = "cpFlat"
-# For Gen 2 support
-config.biasDataProductName = config.connections.bias
-config.flatDataProductName = config.connections.flat
