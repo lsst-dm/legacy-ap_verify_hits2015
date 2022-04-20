c8e37fce20ffa957c634c6d74791f3c5e5c66363
John Parejko <parejkoj@uw.edu>
2022-04-20 14:56:44 -0700

    Cleanup gen2 cruft

--- config/calibrate.py
+++ config/calibrate.py
@@ -1,16 +1,12 @@
 # Config override for lsst.pipe.tasks.calibrate.CalibrateTask
-from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
 
 # Use gaia for astrometry (phot_g_mean for everything, as that is the broadest
 # band with the most depth)
-# Use panstarrs for photometry (grizy filters)
-for refObjLoader in (config.astromRefObjLoader,
-                     config.photoRefObjLoader,):
-    refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
 config.connections.astromRefCat = "gaia"
 config.astromRefObjLoader.ref_dataset_name = config.connections.astromRefCat
 config.astromRefObjLoader.anyFilterMapsToThis = "phot_g_mean"
 
+# Use panstarrs for photometry (grizy filters)
 config.connections.photoRefCat = "panstarrs"
 config.photoRefObjLoader.ref_dataset_name = config.connections.photoRefCat
 config.photoRefObjLoader.filterMap = {
